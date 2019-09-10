27b0111846d97031afe27aad34121f8872fc0ebe
DanielStankovich <danielst2150@gmail.com>
2019-09-10 14:33:13 +0300

    initial commit

---
+++ blogengine/blog/forms.py
@@ -0,0 +1,23 @@
+﻿from django import forms
+from .models import Post
+from django.core.exceptions import ValidationError
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            
+            }
+
+        def clean_slug(self):
+            new_slug = self.cleaned_data['slug'].lower()
+
+            if new_slug == 'create':
+                raise ValidationError('Slug may not be "Create"')
+            return new_slug
